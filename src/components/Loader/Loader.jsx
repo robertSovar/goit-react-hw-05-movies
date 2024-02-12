c98019b7f3cf1b013385db524d593b176f41258e
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-02-12 23:03:21 +0200

    update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,21 @@
-import { BallTriangle } from 'react-loader-spinner';
+import { TailSpin } from 'react-loader-spinner';
 import styles from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={styles.loaderContainer}>
-      <BallTriangle
-        height="100"
-        width="100"
-        color="tomato"
-        ariaLabel="loading"
+      render(
+      <TailSpin
+        visible={true}
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
       />
+      )
     </div>
   );
 };
